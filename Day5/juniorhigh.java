66435c976ec1dfc0375beaf1c4435b547b36aeb7
manasvisrinivas13 <71107218+manasvisrinivas13@users.noreply.github.com>
2020-09-18 15:00:50 -0400

    Add files via upload

---
+++ Day5/juniorhigh.java
@@ -0,0 +1,41 @@
+package school;
+import school.student;
+import school.staff;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import school.founders;
+
+public abstract class juniorhigh
+ { 
+	public abstract void profile();
+
+	public static void main (String[] args) throws IOException 
+	{  
+		{ 
+			founders fo = new founders();
+	        fo.profile();
+	    }
+		
+	float sal;
+	
+		try (Scanner in = new Scanner(System.in))
+	   {
+			{
+				student p = new student();
+			    p.profile();
+			}
+			
+			System.out.println("enter salary of the staff");
+			sal = in.nextFloat();
+		}
+
+		staff s = new staff();
+		s.profile();
+		s.salary(sal);
+		
+		}
+
+	
+}
